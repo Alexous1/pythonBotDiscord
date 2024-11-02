67fe322be8df453e3e46caa6fab9c777ad630429
Alexous1 <123593212+Alexous1@users.noreply.github.com>
2024-11-02 16:11:47 +0100

    Update comments main.py
    big error

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# define the permissions for the bot
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
@@ -14,33 +15,33 @@ status = ['online', 'offline', 'idle', 'dnd', 'invisible']
 
 load_dotenv(dotenv_path="config")
 
-// create an event
+# create an event
 @bot.event
 async def on_ready():
     print("Le bot est connecté.")
 
-// event : when a new people arrive, send a texte on a chanel
+# event : when a new people arrive, send a texte on a chanel
 @bot.event
 async def on_member_join(member):
     print(f"Un nouveau membre est arrivé : {member.display_name}")
     print("test")
 
-// when someone reaction to a message, send in a channel the reaction and who reacted
+# when someone reaction to a message, send in a channel the reaction and who reacted
 @bot.event
 async def on_raw_reaction_add(payload):
     if payload.member.bot:
         return
     
-    // get the information for the event
+    # get the information for the event
     channel = await bot.fetch_channel(payload.channel_id)
     message = await channel.fetch_message(payload.message_id)
     user = await bot.fetch_user(payload.user_id)
     send_channel = await bot.fetch_channel()
 
-    // send the message
+    # send the message
     await send_channel.send(f"{user.display_name} a réagi à  \"{message.content}\" par {payload.emoji}")
 
-// when someone do the command /del x, the bot delete x message
+# when someone do the command /del x, the bot delete x message
 @bot.command(name="del")
 async def delete(ctx, number: int):
     messages = [message async for message in ctx.channel.history(limit=number + 1)]
@@ -48,7 +49,7 @@ async def delete(ctx, number: int):
     for each_message in messages:
         await each_message.delete()
 
-// when someone do the command /clear, the bot clear the channel
+# when someone do the command /clear, the bot clear the channel
 @bot.command(name="clear")
 async def clear(ctx):
     messages = [message async for message in ctx.channel.history(limit=None)]
@@ -56,7 +57,7 @@ async def clear(ctx):
     for each_message in messages:
         await each_message.delete()
 
-// the bot responds how many messages the user has sent
+# the bot responds how many messages the user has sent
 @bot.command(name="send")
 async def message_count(ctx):
 
@@ -69,7 +70,7 @@ async def message_count(ctx):
 
     await ctx.send(f"{user.display_name} a envoyé un total de {total_count} messages sur ce serveur.")
 
-// this command define the statut of the bot
+# this command define the statut of the bot
 @bot.command(name="BotStatus")
 async  def bot_status(ctx, nb: int):
 
@@ -77,7 +78,7 @@ async  def bot_status(ctx, nb: int):
     game = discord.Game("test")
     await bot.change_presence(status=status[nb], activity=game)
 
-// this command get the information about the user
+# this command get the information about the user
 @bot.command(name="UserInfo")
 async def user_info(ctx):
 
@@ -99,5 +100,5 @@ async def user_info(ctx):
         f"{user.display_name} a envoyé {nb_message} messages dans ce serveur.")
 
 
-//run the bot
+# run the bot
 bot.run(os.getenv("TOKEN"))
